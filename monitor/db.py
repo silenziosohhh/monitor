c32263c001b30fb28fdcbe4a8eb8056b255e6cf2
silenziosohhh <lorenzolorigaa08@email.com>
2025-12-30 20:46:20 +0100

    Initial commit

--- monitor/db.py
+++ monitor/db.py
@@ -1,13 +1,17 @@
 import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def get_connection():
     return mysql.connector.connect(
-        host="mysql-1b89fbbc-neirly-1f0b.h.aivencloud.com",
-        port=12792,
-        user="avnadmin",
-        password="AVNS_WFVVRKoptRF2XSo8wm2",
-        database="defaultdb",
-        ssl_disabled=False
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT", 3306)),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        ssl_disabled=os.getenv("DB_SSL_DISABLED", "False").lower() in ("true", "1", "yes")
     )
 
 def get_last_status(cursor, service_id):
